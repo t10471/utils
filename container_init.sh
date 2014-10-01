043440f9225ee3ec11362e301a3a67e9c311fbe9
t10471 <t104711202@gmail.com>
2014-10-02 02:52:46 +0900

    added scala

--- container_init.sh
+++ container_init.sh
@@ -10,7 +10,7 @@ fi
 id=`docker ps |grep $name |awk '{ print $1 }'`
 ip=`docker inspect -f "{{ .NetworkSettings.IPAddress }}" $id`
 
-scp  -i ~/insecure_key ~/.ssh/id_rsa_github root@$ip:~/.ssh/id_rsa_github
+scp  -i insecure_key ~/.ssh/id_rsa_github root@$ip:~/.ssh/id_rsa_github
 
-ssh -i ~/insecure_key root@$ip "sh dotfiles.sh"
+ssh -i insecure_key root@$ip "sh dotfiles.sh"
 
